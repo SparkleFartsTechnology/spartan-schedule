5c0d8b3b1310b1fd5a418820348c0c8f8ab634c7
Sinful Spartan <Sinful.spartan@gmail.com>
2014-05-23 18:38:01 -0500

    Finally got a icon to replace the stock one

--- gen/com/SpartanTech/schedule/R.java
+++ gen/com/SpartanTech/schedule/R.java
@@ -35,7 +35,8 @@ public final class R {
         public static final int ic_action_stat_reply=0x7f020001;
         public static final int ic_action_stat_share=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int ic_stat_assignment=0x7f020004;
+        public static final int ic_launcher_old=0x7f020004;
+        public static final int ic_stat_assignment=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f0a000b;
